a94f74a1499cbf16178494275ef7468e6e95ae48
akbarraga <155419188+akbarraga@users.noreply.github.com>
2024-07-06 09:56:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const MenuIcon = document.getElementById("menu-icon");
+const MenuList = document.getElementById("menu-list");
+
+MenuIcon.addEventListener("click", () => {
+    MenuList.classList.toggle("hidden");
+});
+
+function toggleNavbar() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("hidden");
+  }
